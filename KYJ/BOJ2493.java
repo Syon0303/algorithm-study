f4b6e8d3f0dd93438dcab21c9ac090723d97bc54
YUJIN KWAK <52647838+dbelse@users.noreply.github.com>
2022-02-08 23:36:53 +0900

    [BOJ] 탑 / 골드5 / 3시간
    https://www.acmicpc.net/problem/2493

--- KYJ/BOJ2493.java
+++ KYJ/BOJ2493.java
@@ -8,7 +8,7 @@ public class BOJ2493 {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-        // stack으로 입력 받으면서 풀어야 시간초과 나지 않음
+        	// stack으로 입력 받으면서 풀어야 시간초과 나지 않음
 		Stack<int[]> tops = new Stack<>();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
@@ -21,11 +21,11 @@ public class BOJ2493 {
 					sb.append(tops.peek()[0] + " ");
 					break;
 				}
-                // 현재 탑보다 왼쪽에 있는 탑이 더 작다면, 왼쪽 탐은 영원히 레이저를 수신할 수 없음. 따라서 pop
+                		// 현재 탑보다 왼쪽에 있는 탑이 더 작다면, 왼쪽 탐은 영원히 레이저를 수신할 수 없음. 따라서 pop
 				tops.pop();
 			}
 
-            // 탑이 비었다면 0
+            		// 탑이 비었다면 0
 			if (tops.isEmpty()) {
 				sb.append("0 ");
 			}
