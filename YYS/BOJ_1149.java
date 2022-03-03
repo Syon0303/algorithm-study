94cb8ba1dcbf045c9e20517e3fccfb46e6df1930
devyoseph <josephdev123@naver.com>
2022-03-03 18:05:49 +0900

    [BOJ] RGB거리 / 실버 1 / 15분
    https://www.acmicpc.net/problem/1149

---
+++ YYS/BOJ_1149.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][3];
+        int[][] dp = new int[N][3];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][2] = Integer.parseInt(st.nextToken());
+        }
+        dp[0][0] = arr[0][0];
+        dp[0][1] = arr[0][1];
+        dp[0][2] = arr[0][2];
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+            dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][0]) + arr[i][2];
+        }
+        int MIN = Integer.MAX_VALUE;
+        for (int i = 0; i < 3; i++) {
+            if (MIN > dp[N - 1][i]) {
+                MIN = dp[N - 1][i];
+            }
+        }
+        System.out.println(MIN);
+    }
+}
